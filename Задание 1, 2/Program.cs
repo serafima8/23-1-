18af8c45fdc4db29f002c048f014e721fbc81654
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-05-29 16:15:49 +0300

    Add files via upload

---
+++ Задание 1, 2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тема_23
+{
+    // 1 задание
+
+    enum Month
+    {
+        Январь,
+        Февраль,
+        Март,
+        Апрель,
+        Май,
+        Июнь,
+        Июль,
+        Август,
+        Сентябрь,
+        Октябрь,
+        Ноябрь,
+        Декабрь
+    }
+
+    // 2 задание 
+
+    enum Week
+    {
+        Sat,
+        Sun,
+        Mon,
+        Tue,
+        Wed,
+        Thu,
+        Fri
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1 задание 
+
+            foreach (var i in Enum.GetValues(typeof(Month)))
+            {
+                Console.WriteLine($"{(int)i}\t{i}");
+            }
+
+            // 2 задание
+
+            foreach (var i in Enum.GetValues(typeof(Week)))
+            {
+                Console.WriteLine($"{i} имеет значение {(int)i}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
